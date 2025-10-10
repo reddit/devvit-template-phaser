2d5ae6c9f19f12ae22b7e903c583d0198e6cd920
Edgar Neto <edgar.neto@reddit.com>
2025-10-10 17:18:06 +0100

    Custom splash screen

--- src/server/core/post.ts
+++ src/server/core/post.ts
@@ -8,7 +8,18 @@ export const createPost = async () => {
 
   return await reddit.submitCustomPost({
     splash: {
+      // Splash screen customization
       appDisplayName: '<% name %>',
+      backgroundUri: 'default-splash.png',
+      buttonLabel: 'Start Playing',
+      description: 'An exciting interactive experience',
+      entryUri: 'index.html',
+      heading: 'Welcome to the Game!',
+      appIconUri: 'default-icon.png',
+    },
+    postData: {
+      gameState: 'initial',
+      score: 0,
     },
     subredditName: subredditName,
     title: '<% name %>',
